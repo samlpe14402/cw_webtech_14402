78bcb01b6239cfcf86f97e0260490439a59c8258
samlpe14402 <wiut14402@gmail.com>
2021-12-03 20:47:26 +0500

    changes

---
+++ app.js
@@ -0,0 +1,18 @@
+
+
+function first_credential(){
+    console.log(" ___    __    _  _    __     ___  ____ ");
+    console.log("/ __)  /__\  ( \/ )  /__\   / __)( ___)");
+    console.log("\__ \ /(__)\  \  /  /(__)\ ( (_-. )__) ");
+    console.log("(___/(__)(__)  \/  (__)(__) \___/(____)");
+}
+
+function second_credential(){
+    console.log(" ____      _    __     __    _      ____  _____ ");
+    console.log("/ ___|    / \   \ \   / /   / \    / ___|| ____|");
+    console.log("\___ \   / _ \   \ \ / /   / _ \  | |  _ |  _|  ");
+    console.log(" ___) | / ___ \   \ V /   / ___ \ | |_| || |___ ");
+    console.log("|____/ /_/   \_\   \_/   /_/   \_\ \____||_____|");
+}
+
+second_credential();
